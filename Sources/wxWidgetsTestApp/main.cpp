0b21a00a3fcbe6316e061e9056a1b3a1cea393c8
Viktor Kovacs <viktorkovacs@gmail.com>
2020-12-28 14:31:27 +0100

    Use always the wxWidgets context.

--- Sources/wxWidgetsTestApp/main.cpp
+++ Sources/wxWidgetsTestApp/main.cpp
@@ -320,6 +320,48 @@ public:
 	}
 };
 
+static const NUIE::BasicSkinParams& GetAppSkinParams ()
+{
+	static const NUIE::BasicSkinParams skinParams (
+		/*backgroundColor*/ NUIE::Color (255, 255, 255),
+		/*connectionLinePen*/ NUIE::Pen (NUIE::Color (38, 50, 56), 1.0),
+		/*connectionMarker */ NUIE::SkinParams::ConnectionMarker::Circle,
+		/*connectionMarkerSize*/ NUIE::Size (8.0, 8.0),
+		/*nodePadding*/ 5.0,
+		/*nodeBorderPen*/ NUIE::Pen (NUIE::Color (38, 50, 56), 1.0),
+		/*nodeHeaderTextFont*/ NUIE::Font (L"Arial", 12.0),
+		/*nodeHeaderTextColor*/ NUIE::Color (250, 250, 250),
+		/*nodeHeaderErrorTextColor*/ NUIE::Color (250, 250, 250),
+		/*nodeHeaderBackgroundColor*/ NUIE::Color (41, 127, 255),
+		/*nodeHeaderErrorBackgroundColor*/ NUIE::Color (199, 80, 80),
+		/*nodeContentTextFont*/ NUIE::Font (L"Arial", 10.0),
+		/*nodeContentTextColor*/ NUIE::Color (0, 0, 0),
+		/*nodeContentBackgroundColor*/ NUIE::Color (236, 236, 236),
+		/*slotTextColor*/ NUIE::Color (0, 0, 0),
+		/*slotTextBackgroundColor*/ NUIE::Color (246, 246, 246),
+		/*slotMarker*/ NUIE::SkinParams::SlotMarker::Circle,
+		/*hiddenSlotMarker*/ NUIE::SkinParams::HiddenSlotMarker::Arrow,
+		/*slotMarkerSize*/ NUIE::Size (8.0, 8.0),
+		/*selectionBlendColor*/ NUIE::BlendColor (NUIE::Color (41, 127, 255), 0.25),
+		/*disabledBlendColor*/ NUIE::BlendColor (NUIE::Color (0, 138, 184), 0.2),
+		/*selectionRectPen*/ NUIE::Pen (NUIE::Color (41, 127, 255), 1.0),
+		/*nodeSelectionRectPen*/ NUIE::Pen (NUIE::Color (41, 127, 255), 3.0),
+		/*buttonBorderPen*/ NUIE::Pen (NUIE::Color (146, 152, 155), 1.0),
+		/*buttonBackgroundColor*/ NUIE::Color (217, 217, 217),
+		/*textPanelTextColor*/ NUIE::Color (0, 0, 0),
+		/*textPanelBackgroundColor*/ NUIE::Color (236, 236, 236),
+		/*groupNameFont*/ NUIE::Font (L"Arial", 12.0),
+		/*groupNameColor*/ NUIE::Color (0, 0, 0),
+		/*groupBackgroundColors*/ NUIE::NamedColorSet ({
+			{ NE::LocalizeString (L"Blue"), NUIE::Color (160, 200, 240) },
+			{ NE::LocalizeString (L"Green"), NUIE::Color (160, 239, 160) },
+			{ NE::LocalizeString (L"Red"), NUIE::Color (239, 189, 160) }
+		}),
+		/*groupPadding*/ 10.0
+	);
+	return skinParams;
+}
+
 class MainFrame : public wxFrame
 {
 public:
@@ -332,7 +374,7 @@ public:
 		applicationState ()
 	{
 		NE::StringConverterPtr stringConverter (new NE::BasicStringConverter (NE::GetDefaultStringSettings ()));
-		NUIE::SkinParamsPtr skinParams (new NUIE::BasicSkinParams (NUIE::GetDefaultSkinParams ()));
+		NUIE::SkinParamsPtr skinParams (new NUIE::BasicSkinParams (GetAppSkinParams ()));
 		NUIE::EventHandlerPtr eventHandler (new MyNodeEditorEventHandler (nodeEditorControl));
 		std::shared_ptr<WXAS::NodeEditorUIEnvironment> uiEnvironment = std::shared_ptr<WXAS::NodeEditorUIEnvironment> (
 			new MyNodeEditorUIEnvironment (
